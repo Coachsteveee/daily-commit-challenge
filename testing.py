093cc6094fca923a98244e52d427ab3a0730b18e
ogrigriii <57384448+Coachsteveee@users.noreply.github.com>
2021-09-26 16:23:11 +0800

    uwu

--- testing.py
+++ testing.py
@@ -1,2 +1,2 @@
-for i in range(1,int(input())): 
-    print(*[i*1]*i, sep='', end='\n')
+for i in range(1,input()): 
+    print(*list(int(range(1,i+1))) + list(int(reversed(range(1,i)))), sep='', end='\n')
